6f5f78c0241130255a54de7565af11a7b3204283
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2023-03-04 12:47:30 -0300

    mostrar colocacao filtrando e conceratando escrita

--- app/backend/src/controllers/Leaderboard.controller.ts
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -5,18 +5,24 @@ class UserController {
   private leaderboardService = new LeaderboardService();
 
   public async testes(_req: Request, res: Response): Promise<void> {
-    const t = await this.leaderboardService.test();
-    const s = t.map(({ goalsFavor, goalsOwn, name, totalDraws, totalGames, totalLosses,
-      totalPoints, totalVictories }) => ({ name,
+    const array = await this.leaderboardService.criarArray();
+    const arraySemId = array.map(({ goalsFavor, goalsOwn, name, totalDraws, totalGames, totalLosses,
+      totalPoints, totalVictories, efficiency, goalsBalance }) => ({ name,
       totalPoints,
       totalGames,
       totalVictories,
       totalDraws,
       totalLosses,
       goalsFavor,
-      goalsOwn }));
-    res.status(200).json(s);
+      goalsOwn,
+      goalsBalance,
+      efficiency,
+    }));
+    res.status(200).json(arraySemId);
   }
 }
+
+// const n = new UserController();
+// n.testes();
 
 export default UserController;
--- app/backend/src/services/Leaderboard.service.ts
+++ app/backend/src/services/Leaderboard.service.ts
@@ -5,19 +5,10 @@ import TeamsService from './Teams.service';
 class LeaderboardService {
   private todosTeam: TeamsModel[] = [];
   private zero = 0;
-  private objReiniciado = { id: 1,
-    name: '',
-    totalPoints: 0,
-    totalGames: 0,
-    totalVictories: 0,
-    totalDraws: 0,
-    totalLosses: 0,
-    goalsFavor: 0,
-    goalsOwn: 0 };
 
-  private arr: { id: number; name: string; totalPoints: number; totalGames: number;
+  private arrayTimes: { id: number; name: string; totalPoints: number; totalGames: number;
     totalVictories: number; totalLosses: number; totalDraws: number; goalsFavor: number;
-    goalsOwn: number; }[] = [{
+    goalsOwn: number; goalsBalance: number; efficiency: number }[] = [{
     id: 1,
     name: '',
     totalPoints: 0,
@@ -27,15 +18,17 @@ class LeaderboardService {
     totalLosses: 0,
     goalsFavor: 0,
     goalsOwn: 0,
+    goalsBalance: 0,
+    efficiency: 0,
   }];
 
-  private ass = Matches.findAll({
+  private resultMatches = Matches.findAll({
     where: { inProgress: false },
     order: [['homeTeamId', 'ASC']],
     attributes: { exclude: ['id'] },
   });
 
-  private oooo = { id: 0,
+  private objectResult = { id: 0,
     name: '',
     totalPoints: 0,
     totalGames: 0,
@@ -43,68 +36,90 @@ class LeaderboardService {
     totalDraws: 0,
     totalLosses: 0,
     goalsFavor: 0,
-    goalsOwn: 0 };
+    goalsOwn: 0,
+    goalsBalance: 0,
+    efficiency: 0 };
 
-  private bbb = { id: 0,
+  private objMatches = { id: 0,
     name: '',
     totalPoints: 0,
     totalGames: 0,
     totalVictories: 0,
     totalDraws: 0,
-    totalLosses: 0 };
+    totalLosses: 0,
+    goalsBalance: 0,
+    efficiency: 0 };
 
-  public async pontos(homeTeamGoals: number, awayTeamGoals: number, objj: { totalVictories: number;
-    totalPoints: number; totalLosses: number; totalDraws: number }) {
-    const obj = objj;
-    if (homeTeamGoals > awayTeamGoals) { obj.totalVictories += 1; obj.totalPoints += 3; }
-    if (homeTeamGoals < awayTeamGoals) obj.totalLosses += (this.zero + 1);
-    if (homeTeamGoals === awayTeamGoals) { obj.totalDraws += 1; obj.totalPoints += 1; }
+  public async colocarResultMatches(homeTeamGoals: number, awayTeamGoals: number, obj: {
+    totalVictories: number; totalPoints: number; totalLosses: number; totalDraws: number
+    efficiency: number; totalGames: number }) {
+    const objResult = obj;
+    if (homeTeamGoals > awayTeamGoals) {
+      objResult.totalVictories += 1;
+      objResult.totalPoints += 3;
+    }
+    if (homeTeamGoals < awayTeamGoals) objResult.totalLosses += (this.zero + 1);
+    if (homeTeamGoals === awayTeamGoals) { objResult.totalDraws += 1; objResult.totalPoints += 1; }
+    objResult.efficiency = Number(
+      ((objResult.totalPoints / (objResult.totalGames * 3)) * 100).toFixed(2),
+    );
   }
 
-  public async colocarResultados(homeTeamGoals: number, awayTeamGoals: number, a: boolean) {
+  public async colocarValorMatch(homeTeamGoals: number, awayTeamGoals: number, jaContem: boolean) {
     let obj;
-    const teste = this.arr;
-    if (!a) obj = this.oooo;
-    if (a) obj = this.bbb;
-    if (!a) {
-      this.arr = [...teste, this.oooo];
-      if (obj)obj.id = this.bbb.id;
+    const arrrayAntigo = this.arrayTimes;
+    if (!jaContem) obj = this.objectResult;
+    if (jaContem) obj = this.objMatches;
+    if (!jaContem) {
+      this.arrayTimes = [...arrrayAntigo, this.objectResult];
+      if (obj)obj.id = this.objMatches.id;
     }
-    if (obj && obj.id === this.bbb.id) {
+
+    if (obj && obj.id === this.objMatches.id) {
       if (obj.totalGames === 0 && obj.name !== this.todosTeam[0].teamName) obj.id += 1;
       obj.name = this.todosTeam[obj.id - 1].teamName;
-      this.arr[this.arr.length - 1].goalsFavor += homeTeamGoals;
-      this.arr[this.arr.length - 1].goalsOwn += awayTeamGoals;
+      this.arrayTimes[this.arrayTimes.length - 1].goalsFavor += homeTeamGoals;
+      this.arrayTimes[this.arrayTimes.length - 1].goalsOwn += awayTeamGoals;
+      this.arrayTimes[this.arrayTimes.length - 1].goalsBalance += (homeTeamGoals - awayTeamGoals);
       obj.totalGames += 1;
-      await this.pontos(homeTeamGoals, awayTeamGoals, obj);
+      await this.colocarResultMatches(homeTeamGoals, awayTeamGoals, obj);
     }
   }
 
-  public async somarss(obj: Matches | null) {
-    const arrs = this.arr;
-    const objAtual = arrs[arrs.length - 1];
-    const ids = obj?.homeTeamId;
-    this.bbb = arrs[arrs.length - 1];
+  public async criarObject(obj: Matches | null) {
+    const array = this.arrayTimes;
+    const objAtual = array[array.length - 1];
+    const id = obj?.homeTeamId;
+    this.objMatches = array[array.length - 1];
     if (obj !== null) {
       if (objAtual.id === obj.homeTeamId) {
-        await this.colocarResultados(obj.homeTeamGoals, obj.awayTeamGoals, true);
+        await this.colocarValorMatch(obj.homeTeamGoals, obj.awayTeamGoals, true);
       }
-      if (ids && objAtual.id !== obj.homeTeamId) {
-        const a = this.oooo;
-        a.id = ids;
-        a.name = this.todosTeam[ids - 1].teamName;
-        await this.colocarResultados(obj.homeTeamGoals, obj.awayTeamGoals, false);
+      if (id && objAtual.id !== obj.homeTeamId) {
+        const a = this.objectResult;
+        a.id = id;
+        a.name = this.todosTeam[id - 1].teamName;
+        await this.colocarValorMatch(obj.homeTeamGoals, obj.awayTeamGoals, false);
       }
     }
   }
 
-  public async test() {
-    this.todosTeam = await new TeamsService().findAll();
-    this.arr[0].name = this.todosTeam[0].teamName;
+  public async colocarEmOrder(array: { totalPoints: number; goalsFavor: number;
+    goalsOwn: number; goalsBalance: number; name: string; totalDraws: number; totalGames: number
+    totalVictories: number; efficiency: number; totalLosses: number }[]) {
+    const arrayEmOrdem = array.sort((a, b) => {
+      if (a.totalPoints < b.totalPoints) return 1; if (a.totalPoints > b.totalPoints) return -1;
+      if (a.goalsBalance < b.goalsBalance) return 1; if (a.goalsBalance > b.goalsBalance) return -1;
+      if (a.goalsFavor < b.goalsFavor) return 1; if (a.goalsFavor > b.goalsFavor) return -1;
+      if (a.goalsOwn < b.goalsOwn) return 1; if (a.goalsOwn > b.goalsOwn) return -1; return 0;
+      console.log(this.zero);
+    });
+    return arrayEmOrdem;
+  }
 
-    (await this.ass).forEach(async (e) => {
-      // console.log(e.awayTeamGoals);
-      this.oooo = { id: 0,
+  public async zerarArray() {
+    this.arrayTimes = [
+      { id: 1,
         name: '',
         totalPoints: 0,
         totalGames: 0,
@@ -112,12 +127,31 @@ class LeaderboardService {
         totalDraws: 0,
         totalLosses: 0,
         goalsFavor: 0,
-        goalsOwn: 0 };
-      await this.somarss(e);
-    });
-    const s = this.arr;
-    this.arr = [this.objReiniciado];
-    return s;
+        goalsOwn: 0,
+        goalsBalance: 0,
+        efficiency: 0 },
+    ];
+  }
+
+  public async criarArray() {
+    this.todosTeam = await new TeamsService().findAll();
+    await this.zerarArray();
+    this.arrayTimes[0].name = this.todosTeam[0].teamName;
+    (await this.resultMatches).forEach(async (e) => {
+      this.objectResult = { id: 0,
+        name: '',
+        totalPoints: 0,
+        totalGames: 0,
+        totalVictories: 0,
+        totalDraws: 0,
+        totalLosses: 0,
+        goalsFavor: 0,
+        goalsOwn: 0,
+        goalsBalance: 0,
+        efficiency: 0 };
+      await this.criarObject(e);
+    }); const result = this.arrayTimes;
+    const resultado = await this.colocarEmOrder(result); return resultado;
   }
 }
 export default LeaderboardService;
